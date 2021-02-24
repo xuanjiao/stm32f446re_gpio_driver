e8ce0f57633ac9a48c1b4ae08efa1f4899d6a5c7
xuanjiao <xuanjiao.zhu@gmail.com>
2021-02-24 14:41:00 +0100

    first commit

---
+++ driver/Inc/stm32f446re.h
@@ -0,0 +1,17 @@
+/*
+ * stm32f446re.h
+ *
+ *  Created on: Feb 24, 2021
+ *      Author: Xuanj
+ */
+
+#ifndef INC_STM32F446RE_H_
+#define INC_STM32F446RE_H_
+
+#define FLASH_BASEADDR
+#define SRAM1_BASEADDR
+#define	SRAM2_BASEADDR
+#define ROM
+#define SRAM
+
+#endif /* INC_STM32F446RE_H_ */
