499a67494dcef432754f99a52999d3387444f6f3
xuanjiao <xuanjiao.zhu@gmail.com>
2021-02-28 19:43:53 +0100

    use pin name Macros

--- Src/led_toggle.c
+++ Src/led_toggle.c
@@ -20,7 +20,7 @@ int main()
 
 	// LED2 is connected to PA5
 	gpio_handle.pGPIOx = GPIOA;
-	gpio_handle.GPIO_PinConfig.GPIO_PinNumber = 5;
+	gpio_handle.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
 	gpio_handle.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	gpio_handle.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUT_TYPE_PP;
 	gpio_handle.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_NO_PU_PD;
@@ -31,7 +31,7 @@ int main()
 	GPIO_Init(&gpio_handle);
 
 	while(1){
-		GPIO_ToggleOutputPin(GPIOA, 5);
+		GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
 		delay();
 	}
 	return 0;
