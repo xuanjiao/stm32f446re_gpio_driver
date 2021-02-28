0c720c249f1ef732683c0304444fa96b44814bf4
xuanjiao <xuanjiao.zhu@gmail.com>
2021-02-28 19:59:02 +0100

    typo

--- driver/Inc/stm32f446xx_gpio.h
+++ driver/Inc/stm32f446xx_gpio.h
@@ -109,8 +109,8 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx); 	/*Use reset register to reset the GPIO
  */
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
-void GPIO_WriteToInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber, uint8_t Value);
-void GPIO_WriteToInputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber, uint8_t Value);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
 
 /*
